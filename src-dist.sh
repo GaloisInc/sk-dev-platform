bd53015e990417d739ec45c8c78a1bab81ff30ef
Jonathan Daugherty <jtd@galois.com>
2014-09-10 09:26:34 -0700

    src-dist: do less now that the repository is closer to what we want to release

--- src-dist.sh
+++ src-dist.sh
@@ -25,27 +25,16 @@ bash setup.sh
 git submodule init
 git submodule update
 
-mv $DIST_DIR/dist-configs/grub.conf.sample $DIST_DIR/
-mv $DIST_DIR/docs/programming_guide.txt $DIST_DIR/
-mv $DIST_DIR/docs/README.src.dist $DIST_DIR/README
-mv $DIST_DIR/LICENSE $DIST_DIR/
-
 rm $DIST_DIR/os/build/*.gz
 rm $DIST_DIR/os/build/*.bz2
-rm $DIST_DIR/{src-dist.sh,hudson-build.sh,upload.sh}
+rm $DIST_DIR/src-dist.sh
 rm $DIST_DIR/docs/RELEASES
 rm $DIST_DIR/os/README
 rm -f $DIST_DIR/os/build/buildroot-2014.08/dl.tar
-rm -rf $DIST_DIR/dist-configs
-rm -rf $DIST_DIR/docs
 rm -rf $DIST_DIR/libs
 rm -rf $DIST_DIR/.git
 rm $DIST_DIR/.gitmodules
 find $DIST_DIR -name .gitignore | xargs rm
-
-cd $DIST_DIR/os/build/libdl/ && git remote rm origin
-cd $DIST_DIR/os/build/schedtool-dl/ && git remote rm origin
-cd $DIST_DIR/os/build/linux-deadline/ && git remote rm origin
 
 rm -rf $DIST_DIR/os/build/linux-deadline/.git
 
